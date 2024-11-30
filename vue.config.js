03c0514ce26f31d39ddc51ac7563a0aae140d608
slj1935024637 <1935024637@qq.om>
2024-11-30 15:29:15 +0800

    update2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? '/sljgame2/'
@@ -9,7 +11,27 @@ module.exports = defineConfig({
   configureWebpack: {
     resolve: {
       alias: {
-        '@': require('path').resolve(__dirname, 'src')
+        '@': path.resolve(__dirname, 'src')
+      },
+      fallback: {
+        path: require.resolve('path-browserify')
+      }
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        name: false,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true
+          }
+        }
       }
     }
   }
